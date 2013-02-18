f4f518029c04a457d5d7904838f46ed354346619
Paul05 <falcon.perigee@yahoo.com>
2013-02-18 14:25:11 -0700

    edit for 1st commite. -PS

---
+++ main.c
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.c
+ * Author: Neuro-Transmitter Group
+ *         Frank Liu, Darryl Monroe, Michael Berg, Paul Spaude
+ * Class: CST315 & CST316 ASU Polytechnic
+ *
+ * Purpose: Alg to smooth inputs. *
+ *
+ * Created Spring, 2013
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+
+    return (EXIT_SUCCESS);
+} //end main
+
+//END File main.c
