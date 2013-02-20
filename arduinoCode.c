40dc9fdc68f7c8ebb0bc0e97c0984c09f9bb800d
Michael Berg <michaelberg@Michaels-MacBook-Pro.local>
2013-02-19 17:54:25 -0700

    Added the code to the overall main. Should be able to switch between left and right motor for demonstration now. -MB

--- arduinoCode.c
+++ arduinoCode.c
@@ -18,34 +18,45 @@
 
 const int BUTTON = 12;				//States that the BUTTON is connected to pin 12.
 const int LEFT_MOTOR = 7;			//States that the left MOTOR is connected to pin 7.
-const int RIGHT_MOTOR = 4;			//States that the right MOTOR is connected to pin 4. I think this is a motor pin, yes?
+const int RIGHT_MOTOR = 4;			//States that the right MOTOR is connected to pin 4.
+const int TOGGLE_SWITCH = 8;        //Current default value of the toggle switch being pin 8.
 const int INTERRUPT = 0;			//Uses '0' as the interrupt number, which is set up with pin 2.
-volatile int STATE = HIGH;			//Gives the variable STATE which will change between HIGH and LOW allowing for movement.
+const int INTERRUPT_TWO = 1;        //Uses '1' as the interrupt number, which is set up with pin 3.
+volatile int STATE = LOW;			//Gives the variable STATE which will change between HIGH and LOW allowing for movement.
+volatile int SWITCH = HIGH;
 
 void setup(){							//Initial setup process for pins on input and output.
-  	pinMode(BUTTON, INPUT);	 				//Sets pin 12 as the only input
+  	pinMode(BUTTON, INPUT);	 				//Sets pin 12 as an input
+    pinMode(TOGGLE_SWITCH, INPUT);          //sets pin 8 as another input.
 	pinMode(RIGHT_MOTOR, OUTPUT);				//Sets pin 4 as an output
   	pinMode(LEFT_MOTOR, OUTPUT);	 			//Sets pin 7 as an output
   	attachInterrupt(INTERRUPT, buttonPressed, RISING);	//Sets up the interrupt pin, specifices the called function and what mode to use
+    attachInterrupt(INTERRUPT_TWO, toggleSwitch, CHANGE); //Sets the interrupte of the toggle
 }
 
 void loop(){						//Main loop which is constantly being run on the arduino
 	digitalRead(BUTTON);				//Reads the state of digital pin 12
-	if(STATE == LOW){				//If the STATE is not HIGH then the movement should be halted. 
-		digitalWrite(LEFT_MOTOR, STATE);	//Disables the LEFT_MOTOR
-		digitalWrite(RIGHT_MOTOR, STATE);	//Disables the RIGHT_MOTOR
-	}else{						//Else if the STATE is HIGH continue the predestined movement.
-		goForward();				//Calls the "goForward" function which has both motors going, allowing for forward movement.
-		delay(5000);				//Delays the arduino for 5 seconds (the arduino still goes straight)
-		goLeft();				//Calls the "goLeft" function which turns off the left motor and turns on the right motor so it goes left.
-		delay(5000);				//Delays the arduino for 5 seconds (the arduino still goes left)
-		goRight();				//Calls the "goRight" function which turns off the right motor and turns on the left motor so it goes right.
-		delay(5000);				//Delays the arduino for 5 seconds for the last time (the arduino still goes right)
-	}
+    digitalRead(TOGGLE_SWITCH);         //Reads the state of digital pin 8.
+    if(STATE == HIGH)
+    {
+        if(SWITCH == HIGH){
+            goLeft();
+        }else
+            goRight();
+    }
+}
+
+void toggleSwitch(){
+    SWITCH = !SWITCH;       //Changes which motor to go.
 }
 
 void buttonPressed(){				// function buttonPressed which gets called when an interrupt pin gets triggered
 	STATE = !STATE;		 		// switches variable state to its opposite. From HIGH to LOW and vice versa, allowing the process to stop
+    if(STATE == LOW){
+        digitalWrite(LEFT_MOTOR, STATE);
+        digitalWrite(RIGHT_MOTOR, STATE);
+    }
+    
 }
 
 void goRight(){					//Function gets called as part of the predestined movement
