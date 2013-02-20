db15c23596e3fc9856b2c533a2d744df7f7c7fe6
Michael Berg <michaelberg@Michaels-MacBook-Pro.local>
2013-02-19 17:46:12 -0700

    Added toggleSwitch.c -MB

---
+++ toggleSwitch.c
@@ -0,0 +1,23 @@
+/*
+ * File:   right.c
+ * Author: Neuro-Transmitter Group
+ *         Frank Liu, Darryl Monroe, Michael Berg, Paul Spaude
+ * Class: CST315 & CST316 ASU Polytechnic
+ *
+ * Purpose:
+ *
+ * Created Spring, 2013
+ *
+	ORIGINAL AUTHOR: Darryl Monroe
+	EDITOR: Michael Berg
+	DESCRIPTION: This code is designed to have an arduino go in a pattern of *Forward->Left->Right* and it will continue
+			this pattern every 5 seconds.
+	VERSION: 1.0.2
+	DATE: Friday, February 15, 2013
+*/
+
+void toggleSwitch(){
+    SWITCH = !SWITCH;
+}
+ 
+//END file arduinoCode.c
