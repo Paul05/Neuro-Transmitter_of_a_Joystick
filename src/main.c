a43c8f9ac375997160bae6c442a6d098d8e1a62d
Paul05 <falcon.perigee@yahoo.com>
2013-03-01 15:33:21 -0700

    in process of uploading serial and main changes

--- src/main.c
+++ src/main.c
@@ -4,7 +4,14 @@
  *         Frank Liu, Darryl Monroe, Michael Berg, Paul Spaude
  * Class: CST315 & CST316 ASU Polytechnic
  *
- * Purpose: Alg to smooth inputs. *
+ * Purpose: Main takes two inputs the first is the name of the serial port the
+ *    Arduino is connected to and the next is the baud rate for the connection/
+ *    Standard baud rate is 9600.
+ *
+ * This program then takes inputs from the keyboard (wasd) or others depending on
+ * specifications and sends that input to the Arduino after it normalizes the input
+ * to provide a better user experience.
+ *
  *
  * Created Spring, 2013
  */
@@ -16,22 +23,70 @@
 #include "forward.h"
 #include "usbSerialComm.h"
 
+
+
+/**
+ * Welcome message function that displays a welcome message on the console.
+ */
+void welcomeMessage(void)
+{
+    printf("Welcome to the 'Neuro-Trasmitter Of A Joystick' Group's "
+            "Emotiv Normalization Program 1.0!\n\n");
+}
+
+
+/**
+ * Exit message function that displays exit message and keeps console open
+ * until key is pressed on keyboard.
+ */
+void exitMessage(void)
+{
+    printf("\n\nGoodbye\n\n");
+    printf("\n\nPlease press 'ENTER' to exit.\n\n\n");
+    fflush(stdout);
+    (void)getchar();
+}
+
+
+/**
+ * This is the Main function that calls other functions and 'runs' the program.
+ * It needs two arguments on the command line.
+ * First is serial port name (string) and the next is the baud rate (integer -
+ *   9600 is standard).
+ * @param argc
+ * @param argv
+ * @return
+ */
 int main(int argc, char** argv)
 {
-    printf("This is the neuro-transmitter group's project!\n\n");
+    int baudRate = 0;
+    char* serialPortName;
+
+    welcomeMessage();
+
+    if (argc != 2)
+    {
+        printf("\nImproper number of command line arguments!\n");
+        printf("\tUsage: NeuroControlProgram.exe Port BaudRate\n");
+        exitMessage();
+        return(EXIT_FAILURE);
+    }
+    else
+    {
+        serialPortName = argv[1];
+        baudRate = atoi(argv[2]);
+    }
+
+    
 
     left();
     right();
     forward();
     usbSerialComm();
 
-    printf("\nGoodbye!\n\n");
-
-	printf("\n\nPlease press 'ENTER' to exit.\n\n\n");
-	fflush(stdout);
-	(void)getchar();
-	
+    exitMessage();
     return (EXIT_SUCCESS);
+ 
 } //end main
 
 //END File main.c
--- src/usbSerialComm.c
+++ src/usbSerialComm.c
@@ -8,8 +8,49 @@
  *
  * Created Spring, 2013
  */
+
+
+#include <Windows.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <conio.h>
+
+
+/*
+ One to open the correct serial port correctly.
+One to read data from the serial port.
+One to send data to the serial port.
+One to close the serial port when you are done.
+ */
+
+
+void setupCommunication()
+{
+    HANDLE hDevice = CreateFile(L"COM2",GENERIC_READ | GENERIC_WRITE,FILE_SHARE_READ | FILE_SHARE_WRITE,NULL,OPEN_EXISTING,0,0);
+
+    char test[10] = "crap";
+    if (hDevice !=INVALID_HANDLE_VALUE)
+    {
+        printf("Port opened! \n");
+        DCB lpTest;
+        GetCommState(hDevice,&lpTest);
+        lpTest.BaudRate = CBR_9600;
+        lpTest.ByteSize = 8;
+        lpTest.Parity = NOPARITY;
+        lpTest.StopBits = ONESTOPBIT;
+        SetCommState(hDevice,&lpTest);
+
+        DWORD btsIO;
+
+        WriteFile(hDevice,test,strlen(test),&btsIO,NULL);
+
+        CloseHandle(hDevice);
+    }
+
+
+}
+
+
+
 
 void usbSerialComm()
 {
