2fada90125dce85c32947502a6c1e58d2bd69c7b
Paul05 <falcon.perigee@yahoo.com>
2013-03-04 01:03:53 -0700

    finished user input and main c files with headers

--- src/main.c
+++ src/main.c
@@ -4,33 +4,33 @@
  *         Frank Liu, Darryl Monroe, Michael Berg, Paul Spaude
  * Class: CST315 & CST316 ASU Polytechnic
  *
- * Purpose: Main takes two inputs the first is the name of the serial port the
+ * Inputs: Main takes two inputs the first is the name of the serial port the
  *    Arduino is connected to and the next is the baud rate for the connection/
- *    Standard baud rate is 9600.
+ *    Standard baud rate is 9600. If not found on the cmd line, the program will
+ *    ask the user to enter those values with more error checking.
  *
- * This program then takes inputs from the keyboard (wasd) or others depending on
- * specifications and sends that input to the Arduino after it normalizes the input
+ * This program then sets up communication via serial to an arduino. The
+ * connection may in fact be USB but this program uses serial as Arduino
+ * uses USB but mimics a serial port and communication. If the tests complete
+ * and communication works, then this program takes inputs from the keyboard
+ * (wasd) or others depending on specifications.
+ * It then sends that input to the Arduino after it normalizes the input
  * to provide a consistent and usable user experience for an Emotiv controlled
- * wheelchair.
+ * wheelchair. In essence the Emotiv program will use "keystrokes" to communicate
+ * with the Arduino after normalization and the Arduino will convert the input
+ * into usable turning and movement instructions which trip various relays on
+ * an electric wheelchair.
  *
  * Created Spring, 2013
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "usbSerialComm.h"
+#include "userInput.h"
 #include "left.h"
 #include "right.h"
 #include "forward.h"
-#include "usbSerialComm.h"
-
-
-#define BUFFERLENGTH 128    //arbitrary size picked as standar input buffer length
-#define STDPORTLENGTH 5    //size of the standard 1-9 com ports plus null
-#define LRGERPORTLENGTH 6    //size of 10+ com ports plus null
-#define DEVICENAMEAPPEND "\\\\.\\"   //device or "DOS" name for serial port (this is backslashed for C, regular is: '\\.\COMXX'
-#define USEREXITCOMMAND "exit"   //string the user is to type to exit out of input loops/program
-#define LWRBAUDRATE 110      //lower possible baud rate
-#define UPPRBAUDRATE 256000  //upper possible baud rate 
 
 
 /**
@@ -47,6 +47,9 @@ void welcomeMessage(void)
 /**
  * Exit message function that displays exit message and keeps console open
  * until key is pressed on keyboard.
+ *
+ * Note: Is platform specific and uses getchar()!
+ * 
  */
 void exitMessage(void)
 {
@@ -57,124 +60,6 @@ void exitMessage(void)
 
 }//end exitMessage
 
-
-/**
- * If the portName and baudRate are not specified on the command line,
- * this function will get those values from the user and try to get the
- * correct input within reason. It returns a -1 if the user wishes to exit, 0
- * for failure and 1 for success. Furthrmore, the input parameters of the string
- * and int are designed to be returned to the calling function.
- */
-int handleUserInput(char outPortName[], int *outBaudRate)
-{
-    //TODO Refactor into new file (Create file and header)
-    char inputLine[BUFFERLENGTH];  //this is used as buffer for raw user input   
-    int errorFlag;     //status of input 1 = error keep going, 0 = stop success
-    int length;    //var for length of buffer returned
-
-    do
-    {
-        errorFlag = 0;
-        printf("\nPlease enter the port name to connect with the Arduino: ");
-
-        if (fgets(inputLine, sizeof(inputLine), stdin))
-        {
-            length = strlen(inputLine);  //get length of input 
-
-            if (inputLine[length-1] == '\n')
-            {
-                inputLine[length-1] = 0; //replace newline fgets puts in
-            }
-
-            if (strcasecmp(inputLine,USEREXITCOMMAND) == 0)  //compare ignore case
-            {               
-                return -1; //user wishes to exit
-            }
-            else if ( strlen(inputLine) == STDPORTLENGTH ) //if ports 1-9
-            {
-                //TODO Error Here See if length correct!!!
-                printf("OutputRawSTD= %s.\n", inputLine);
-                strncpy(outPortName,inputLine,STDPORTLENGTH);
-                 printf("OutputSTD= %s.\n", outPortName);
-
-            }
-            else if (strlen(inputLine) == LRGERPORTLENGTH) //if ports 10+
-            {
-                printf("OutputRawLRG= %s.\n", inputLine);
-                strncpy(outPortName,DEVICENAMEAPPEND,strlen(DEVICENAMEAPPEND));
-                strncat(outPortName,inputLine,LRGERPORTLENGTH);
-                printf("OutputLRG= %s. \n", outPortName);
-            }          
-            else
-            {
-                printf("\n*Please enter one string for the port being used by "
-                        "the arduino of the proper length. "
-                        "\n\t*Example: 'COM1'. \n\n");
-                errorFlag = 1; //too many strings entered or too long etc.
-            }
-        }
-        else
-        {
-            printf("\n*Error in port name being entered. \n\t*Please try again or "
-                    "type 'exit' to end the program.\n\n");
-            errorFlag = 1; //usually no input found (blank)
-        }
-
-    }while(errorFlag == 1); //end loop get port name
-
-    do
-    {
-        errorFlag = 1;
-        printf("\nPlease enter the intended baud rate of the connection to "
-                "the Arduino: ");
-
-        if (fgets(inputLine, sizeof(inputLine), stdin))
-        {
-            length = strlen(inputLine); //get length of input
-
-            if (inputLine[length-1] == '\n')
-            {
-                inputLine[length-1] = 0; //replace newline fgets puts in
-            }
-
-            if (strcasecmp(inputLine,USEREXITCOMMAND) == 0)  //compare ignore case
-            {
-                return -1; //user wishes to exit
-            }
-            else if (1 == sscanf(inputLine, "%d", &outBaudRate))  //int is found
-            {
-                printf("Output Baud Rate= %d. \n", outBaudRate);
-
-                if (*outBaudRate < LWRBAUDRATE || *outBaudRate > UPPRBAUDRATE ) //check upper and lower possible baud rate bounds
-                {
-                    printf("*Please enter a valid baud rate. "
-                            "(9600 is default) \n\n");
-                    errorFlag = 1; //baud rate is invalid
-                }
-                else
-                {
-                    return 1; //baud rate is valid success
-                }
-            }
-            else
-            {
-                printf("\n*Please enter one number for the baud rate of "
-                        "the connection. \n\t*Example: '9600'. \n\n");
-                errorFlag = 1; //too many ints entered etc.
-            }
-        }
-        else
-        {
-            printf("\n*Error in baud rate entered. Please try again or "
-                    "type 'exit' to end the program.\n\n");
-            errorFlag = 1; //improper input (probably blank)
-        }
-
-    }while(errorFlag == 1); //end loop get baud rate
-
-    return 0; //fall through return (in this case) a neutral number
-
-} //end handleUserInput
 
 
 /**
@@ -187,53 +72,95 @@ int handleUserInput(char outPortName[], int *outBaudRate)
  * @return
  */
 int main(int argc, char** argv)
-{
-    int successFlag = 0;
+{   
     char portName[13];
-    int  baudRate;
+    int  baudRate = 0;
+    int successFlag = 0;
+    char emotivInput = '\0';
 
     welcomeMessage();
 
     if (argc < 2)
     {
-       successFlag = handleUserInput(portName, &baudRate); //inputs not on cmd line ask user
+       successFlag = getPortName(portName); //inputs not on cmd line ask user
+
+       if (successFlag == 1)
+       {
+            baudRate = getBaudRate(); //ask user for baud rate
+            successFlag = baudRate; //checks user input back from getBaudRate()
+       }
     }
     else
     {
         if (strlen(argv[1]) == LRGERPORTLENGTH)
         {
-            strncpy(portName,DEVICENAMEAPPEND,7); //
-            strncat(portName,argv[1],strlen(argv[1]));
+            strncpy(portName,DEVICENAMEAPPEND,strlen(DEVICENAMEAPPEND)); //put device name header first
+            strncat(portName,argv[1],strlen(argv[1])); //append port name to make complete device name
         }
         else if ( strlen(argv[1]) == STDPORTLENGTH )
         {
-            strncpy(portName,argv[1],strlen(argv[1]));
+            strncpy(portName,argv[1],strlen(argv[1])); //put the cmd arg into the portName var
         }
         else
         {
-            printf("Warning invalid port name in: %s \n", argv[1]);
-            exitMessage();
-            return(EXIT_FAILURE);
-        }
+            printf("Warning invalid port name in: %s \n", argv[1]); //portname not correct length or otherwise invalid
+            successFlag = 0;
+
+        } //end if else get values from cmd line
+
+    } //end if else check cmd args or get user input
+
+
+    if (successFlag == -1 || successFlag == 0) //functions to get input from user failed or user wished to exit
+    {
+        exitMessage(); //exit message
+        return(EXIT_FAILURE); //exit program with failure
     }
+    else
+    {
+        printf("\nSetting up communication with Arduino on port: %s at rate %i... \n\n", portName, baudRate);
+
+        //Setup Comm (Open port make sure all is ok)
+
+        //Test Comm  (Send something and recieve something)  char T for example
+
+        //Test Actual Movement (Wheelchair turns left then right for example, test functions etc.)
+        
+    } //end if else check if inputs completed and setup communications/test communications
+
 
     if (successFlag == -1 || successFlag == 0)
     {
         exitMessage();
-        return(EXIT_FAILURE);
+        return(EXIT_SUCCESS);
     }
+    else
+    {
+        printf("\nProgram ready and in loop for Emotiv Input or press 'x' to exit! \n\n");
+
+        do
+        {
+            emotivInput = getCharConsole(); //get input from console
+
+            //Switch Statement
+
+        } while( emotivInput != 'x' ); //end loop get input from console from emotiv device
 
 
-    //setupSuccessFlag = setupCommunication(argv[1],atoi(argv[2]));
-    printf("Setting up communication with Arduino on port: %s at rate %s... \n", portName, baudRate);
+        if (emotivInput == 'x')
+        {
+            printf("\nExit character found now exiting the program! \n\n");
+        }
+        else
+        {
+            printf("\nUnexpected character or action found! Last char: %c. "
+                    "Now Exiting!\n\n", emotivInput);
+        }
 
-    left();
-    right();
-    forward();
-    
-
-    exitMessage();
-    return (EXIT_SUCCESS);
+        exitMessage();
+        return (EXIT_SUCCESS);
+        
+    } //end if else check tests complete and perform get input and normalization loop
  
 } //end main
 
---
+++ src/userInput.h
@@ -0,0 +1,35 @@
+/*
+ * File:   userInput.h
+ * Author: Neuro-Transmitter Group
+ *         Frank Liu, Darryl Monroe, Michael Berg, Paul Spaude
+ * Class: CST315 & CST316 ASU Polytechnic
+ *
+ * Purpose: This file contains functions to get user input from the console.
+ *  The functions in order: get a string port name, an integer baud rate, as
+ *  well as a character, all from the console. The port name and baud rate
+ *  handle limited error checking and limited bound checking for those inputs
+ *  with constants defined in usbSerialComm.h. 
+ *
+ * Created Spring, 2013
+ */
+
+#ifndef USERINPUT_H
+
+ #ifndef BUFFERLENGTH
+    #define BUFFERLENGTH 128    //arbitrary size picked as standar input buffer length
+ #endif
+ #ifndef USEREXITCOMMAND
+    #define USEREXITCOMMAND "exit"   //string the user is to type to exit out of input loops/program
+ #endif
+
+#define	USERINPUT_H
+
+int getPortName(char outPortName[]);
+int getBaudRate(void);
+char getCharConsole(void);
+
+#endif	/* USERINPUT_H */
+
+//END file userInput.h/
+
+
