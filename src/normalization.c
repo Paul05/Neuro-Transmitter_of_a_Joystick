16081c85a8cf7d512551dff0ec45af68dd5b88b8
Michael Berg <michaelberg@Michaels-MacBook-Pro.local>
2013-04-09 20:36:56 -0700

    Refactored the heck out of the normalization. It should be simplified and working now, but I cannot guarantee that.-MB

--- src/normalization.c
+++ src/normalization.c
@@ -26,6 +26,59 @@
 
 int g_count = 0;
 char g_last_userInpt[3];
+/*
+ * The below method checks to see if the input key is a default key you have.
+ * If it is, it returns true else it returns false.
+ */
+int aKey(char key){
+    int theVal = 0;
+    if(key == extG_controllerForwardCmd){
+        theVal = 1;
+    }else if(key == extG_controllerBackCmd){
+        theVal = 1;
+    }else if(key == extG_controllerLeftCmd){
+        theVal = 1;
+    }else if(key == extG_controllerRightCmd){
+        theVal = 1;
+    }else if (key == extG_controllerTestCmd){
+        theVal = 1;
+    }else if(key == extG_controllerExitCmd){
+        theVal = 1;
+    }else{
+        theVal = 0;
+    }
+    return theVal;
+}
+/*
+ * The below method goes through a rigorous if/else loop that checks the inputs.
+ * If the last three inputs are the same it will send it to the arduino and reset.
+ * If they were not the same, it sets the values in appropriate places and tries again.
+ */
+void normalizationAlgorithm(char toSend){
+    if(g_count == 0){
+        g_last_userInpt[0] = toSend;
+    }else if(g_count == 1){
+        g_last_userInpt[1] = toSend;
+    }else if(g_count == 2){
+        g_last_userInpt[2] = toSend;
+    }
+    
+    if(g_last_userInpt[0] == g_last_userInpt[1] && g_last_userInpt[0] == g_last_userInpt[2]){
+        sendToWheelChairController(toSend);
+        delayProgram(400);
+        g_count = 2;
+    }else if(!isnan(g_last_userInpt[2])){
+        if(g_last_userInpt[1] == g_last_userInpt[2]){
+            g_last_userInpt[0] = g_last_userInpt[2];
+            g_count = 2;
+        }else{
+            g_last_userInpt[0] == g_last_userInpt[2];
+            g_count = 1;
+        }
+    }else{
+        g_count++;
+    }
+}
 
 /*
  * This function hands off functionality to the appropriate normalization/direction
@@ -34,66 +87,11 @@ char g_last_userInpt[3];
  */
 void callNormalizeDirectionFuncs(char key)
 {
-     
-    if ( key == extG_controllerForwardCmd )
-    {
-        goForward();
-    }
-    else if ( key == extG_controllerBackCmd )
-    {
-        goBackward();
-    }
-    else if ( key == extG_controllerLeftCmd )
-    {
-        goLeft();
-    }
-    else if ( key == extG_controllerRightCmd )
-    {
-        goRight();
-    }
-    else if ( key == extG_controllerTestCmd )
-    {
-        testControllerCommunication();
-    }
-    else if ( key == extG_controllerExitCmd )
-    {
-        printf("*Exit char recieved %c. \n\n", extG_controllerExitCmd);       
-    }
-    else
-    {
-         printf("\n*Error char not recognized! Char input: %c. \n\n", key); //char not recognized print error, will keep going though
+    if(aKey(key)){ //confirms if the key is one of your default keys
+        normalizationAlgorithm(key); //normalizes the input
+    }else{
+        printf("\n*Error char not recognized! Char input: %c. \n\n", key); //char not recognized print error, will keep going though
     }//end switch on key
-
+    
 } //end callNormalizeDirectionFuncs function
-
-void normalizationAlgorithm(){
-//    if(g_count == 0){
-//        g_last_userInpt[0] = toSend;
-//    }else if(g_count == 1){
-//        g_last_userInpt[1] = toSend;
-//    }else if(g_count == 2){
-//        g_last_userInpt[2] = toSend;
-//    }
-//    if(g_last_userInpt[0] == g_last_userInpt[1] && g_last_userInpt[0] == g_last_userInpt[2]){
-//        g_count = 0;
-//        char tempToSend[] = {toSend};
-//        WriteFile(g_controlDevice,tempToSend,strlen(tempToSend),&g_btsIO,NULL); //writes char to arduino
-//        ++g_count;
-//    }else if(!isnan(g_last_userInpt[2])){
-//        if(g_last_userInpt[0] == g_last_userInpt[1]){
-//            g_last_userInpt[0] == g_last_userInpt[2];
-//            g_count = 1;
-//        }else if(g_last_userInpt[1] == g_last_userInpt[2]){
-//            g_last_userInpt[0] = g_last_userInpt[2];
-//            g_count = 2;
-//        }else{
-//            g_count = 0;
-//            printf("Let's start over.");
-//        }
-//    }else{
-//        g_count++;
-//    }
-}
-
-//END file normalization.c
 
